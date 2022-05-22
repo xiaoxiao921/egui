ce0887f1dc5625fbf59e27d4a654a2c8dff4e30f
xiaoxiao921 <xiaoxiao921@hotmail.fr>
2022-05-23 01:33:37 +0200

    centered window

--- eframe/src/native/run.rs
+++ eframe/src/native/run.rs
@@ -56,7 +56,7 @@ pub fn run_glow(
         }
 
         for ele in event_loop.available_monitors() {
-            let centered_pos = ele.size().to_logical::<f32>(ele.scale_factor());
+            let mut centered_pos = ele.size().to_logical::<f32>(ele.scale_factor());
             let is_not_web = centered_pos.width != 0.0 && centered_pos.height != 0.0;
             if is_not_web {
                 centered_pos.height /= 2.;
