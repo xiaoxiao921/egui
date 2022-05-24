46e3311fe8a640fa3eb4d91f68f41962f95c9b0e
xiaoxiao921 <xiaoxiao921@hotmail.fr>
2022-05-24 19:46:09 +0200

    custom window title separated from app_name

--- eframe/src/native/run.rs
+++ eframe/src/native/run.rs
@@ -76,7 +76,7 @@ pub fn run_glow(
         .with_title(if native_options.window_title.is_some() {
             native_options.window_title.unwrap()
         } else {
-            app_name
+            app_name.to_string()
         });
     let (gl_window, gl) = create_display(native_options, window_builder, &event_loop);
     let gl = std::rc::Rc::new(gl);
